ee3e609b8a55f93e6e580299b95dac6a58cabadb
Aleksandr Shelukheev <shelukheev@gmail.com>
2021-11-28 06:44:36 -0500

    updated some little shit

--- Group33_COMP306_GroupProject/RecipeWebApp/Models/Recipe.cs
+++ Group33_COMP306_GroupProject/RecipeWebApp/Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,21 @@ namespace RecipeWebApp.Models
     public class Recipe
     {
         public int Id { get; set; }
+        [DisplayName("Recipe name")]
         public string RecipeName { get; set; }
+        [DisplayName("Recipe type")]
         public string RecipeType { get; set; }
+
         public string Description { get; set; }
+
+        [DisplayName("Image")]
         public string ImageIrl { get; set; }
+
+        [DisplayName("Author")]
         public string AuthorId { get; set; }
-        public Ingredient[] Ingredients { get; set; }
+
+        [DisplayName("Ingredients")]
+        public List<Ingredient> Ingredients { get; set; }
 
         
     }
